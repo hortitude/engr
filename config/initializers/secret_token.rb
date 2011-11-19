cf2bcab602fe09c58210e76efd87dde47fc54393
Hortitude <hortitude@hortitude.com>
2011-11-19 15:32:51 -0800

    initial checkin

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Starterkit::Application.config.secret_token = 'c37167952d31e255b41d9b5543d42ef7da6212892d9e1404d05a14b9fc7b95d11ce4a5999448d41d35a29a5317526dceddf3e4b988c27ae9f961761234394a3c'
