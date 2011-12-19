d2633d325c8e7d529fa6680f39f05a6df19b813e
Hortitude <hortitude@hortitude.com>
2011-12-19 13:54:18 -0800

    env comment

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,7 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+#use a local config file for params that shouldn't be shared with an open source project
 APP_CONFIG = YAML.load_file("#{Rails.root}/config/local.yml")
 
 # Initialize the rails application
